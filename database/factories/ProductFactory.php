7383e2a59da8ba210b2a6de6f867c53c68359871
hoda <hodah5948@gmail.com>
2018-11-28 19:41:31 +0200

    after seed factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\model\product::class, function (Faker $faker) {
     return [
-        //
+        'name'      => $faker->word,
+        'detail'    => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+        'price'     => $faker->numberBetween($min = 100, $max = 1000),
+        'stock'     => $faker->randomDigit,
+        'discount'  => $faker->numberBetween($min = 2, $max = 30)
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,14 @@
 <?php
-
+use App\model\product;
 use Faker\Generator as Faker;
 
 $factory->define(App\model\review::class, function (Faker $faker) {
     return [
-        //
+        'product_id'    => function () {
+        return product::all()->random();
+        },
+        'customer'      => $faker->word,
+        'review'        => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+        'star'          =>$faker->numberBetween($min = 1, $max = 5)
     ];
 });
