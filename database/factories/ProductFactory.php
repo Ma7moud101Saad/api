c9c27d6e4c90f7ba2ac251da4a8c5a595779ae79
hoda <hodah5948@gmail.com>
2018-11-28 16:54:21 +0200

    created model -a

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\model\product::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\model\review::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
