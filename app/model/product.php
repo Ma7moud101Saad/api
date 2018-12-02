3af677a3b1c2b324de2a96e24ff03af788b1182f
hoda <hodah5948@gmail.com>
2018-12-02 13:34:43 +0200

    product review realtionship

--- app/model/product.php
+++ app/model/product.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\model;
-
+use App\model\review;
 use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
 {
-    //
+    public function reviews() 
+    {
+       return $this->hasMany(review::class); 
+    }
 }
--- app/model/review.php
+++ app/model/review.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\model;
-
+use App\model\product;
 use Illuminate\Database\Eloquent\Model;
 
 class review extends Model
 {
-    //
+    public function product() 
+    {
+        return $this->belongsTo(product::class);
+    }
 }
