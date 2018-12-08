01037d43704db3d9493b86ced962ff296e347095
hoda <hodah5948@gmail.com>
2018-12-08 14:35:02 +0200

    passport installed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {        
-        return productCollection::collection(product::all());
+        return productCollection::collection(product::paginate(10));
     }
 
     /**
